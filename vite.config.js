10c9c627f76214bfc6da62e175079bb94758af33
kevinvillajim <kevinvillajim@hotmail.com>
2024-02-10 02:03:59 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  optimizeDeps: {
-    include: ["@emotion/react", "@emotion/styled"],
-  },
+	plugins: [react()],
+	base: "https://kevinvillajim.github.io/conecta360FrontEnd/",
+	optimizeDeps: {
+		include: ["@emotion/react", "@emotion/styled"],
+	},
 });
